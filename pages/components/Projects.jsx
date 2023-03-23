c6589962ad78f44418547a33e501ab9784b14584
André Fernandes <andrefersouza@gmail.com>
2023-03-23 18:19:01 -0300

    correção do titulo

--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -18,7 +18,7 @@ const Projects = () => {
     <div id='projects' className='w-full'>
       <div className='max-w-[1240px] mx-auto px-2 py-16'>
         <p className='text-xl tracking-widest uppercase text-[#1cff81]'>
-          Projects
+          Projetos
         </p>
         <h2 className='py-4'>O que eu já fiz</h2>
         <div className='grid md:grid-cols-2 gap-8'>
