18c7af1e8f4ec136d7566a207f3c056df18f7b50
Dredeco <Dredeco@users.noreply.github.com>
2024-06-10 16:27:42 +0000

    deploy: 10394a40b4c4e7349fae7feed2c84cea586b7d2c

---
+++ _next/static/chunks/pages/components/Main-ed44b18f3c5278a7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[284],{2469:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Main",function(){return u(2176)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=2469)}),_N_E=n.O()}]);
